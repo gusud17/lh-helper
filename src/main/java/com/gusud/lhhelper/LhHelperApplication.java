2d800109ae15de410ed4cde812c598f1bc0a16a3
gusud17 <gusud96@gmail.com>
2020-05-21 23:57:58 +0900

    create project

---
+++ src/main/java/com/gusud/lhhelper/LhHelperApplication.java
@@ -0,0 +1,13 @@
+package com.gusud.lhhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gusud/lhhelper/LhHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusud.lhhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
